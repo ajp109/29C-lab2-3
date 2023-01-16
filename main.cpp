3fd2f9b5504664a0fa98d855ce0a2ebb2982aa65
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-16 11:49:59 +0000

    Add project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "mbed.h"
 
 int main() {
+
+    int numbers[] = {1,3,5,7,9};
+
+    numbers[0] = 11;
+    numbers[3] = 42;
+
+    for (int i = 0; i < 5; ++i) {
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
+
     while (true) {
         sleep();
     }
